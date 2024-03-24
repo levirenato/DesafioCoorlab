ad6a9fbbeff21a146f7ed126f92fa15b7f6a1469
levi Renato <levirenato@hotmail.com>
2024-03-24 18:19:47 -0300

    feat: script Ok

--- RESULT.md
+++ RESULT.md
@@ -1,2 +1,1 @@
 # Resultado - Desáfio CoorLab
-
--- app/run.sh
+++ app/run.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-echo "Implemente aqui o script para executar a sua solução"
+#!/bin/bash
+
+# Diretório do frontend
+frontend_dir="/app/frontend"
+
+# Diretório do backend
+backend_dir="/app/backend"
+
+# Comandos para o frontend
+echo "Iniciando o frontend..."
+cd "$frontend_dir" && npm install # Navega até o diretório do frontend e instala as dependências
+npm run dev # Executa o servidor de desenvolvimento do Vue.js
+
+# Comandos para o backend
+echo "Iniciando o backend..."
+cd "$backend_dir" && pip install -r requirements.txt # Navega até o diretório do backend e instala as dependências
+python run.py # Inicia o servidor do FastAPI
